9d269181a7c8e827f4277f76a2d06f3b34884f2d
AdityaPudasaini <workingaditya00@gmail.com>
2026-02-26 20:01:05 +0545

    Creating GymGUI.java

---
+++ Code/GymGUI.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class GymGUI
+{
+    public static void main(String[] args)
+    {
+        ArrayList<GymMember> arrayInt = new ArrayList<GymMember>(); // ArrayList for GymMember variables
+    }
+}
